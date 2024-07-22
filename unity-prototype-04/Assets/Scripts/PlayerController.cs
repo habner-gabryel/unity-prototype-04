558da445ba2af084e758bdd1dba19818966c6c27
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-22 20:59:30 -0300

    4.3.6 commit

--- unity-prototype-04/Assets/Scripts/PlayerController.cs
+++ unity-prototype-04/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5F;
     public bool hasPowered;
     private float powerupStrength = 15F;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +21,16 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter(Collider other){
         if(other.CompareTag("Powerup")){
             hasPowered = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountDownRoutine());
         }
     }
 
-    IEnumetator PowerupCountDownRoutine(){
+    IEnumerator PowerupCountDownRoutine(){
         yield return new WaitForSeconds(7);
         hasPowered = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision){
@@ -44,7 +47,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerupIndicator.transform.position = playerRb.transform.position - new Vector3(0F,0.5F,0F);
     }
 }
