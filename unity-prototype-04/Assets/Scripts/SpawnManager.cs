25c2e07d44cbbf5d5b4a607e37cee1bb6e8b2492
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-22 21:13:24 -0300

    4.4.4 commit

--- unity-prototype-04/Assets/Scripts/SpawnManager.cs
+++ unity-prototype-04/Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 10;
     public int enemyCount;
+    public int waveNumber = 1;
     
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
     }
 
     void SpawnEnemyWave(int enemiesToSpawn){
@@ -34,7 +35,8 @@ public class SpawnManager : MonoBehaviour
         enemyCount = FindObjectsOfType<EnemyController>().Length;
 
         if(enemyCount == 0){
-            SpawnEnemyWave(1);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
         }
     }
 }
